e96f56c4e7f6c46454910c602d571d628b718a6b
Abdullah <162957435+AbdullahNisar123@users.noreply.github.com>
2025-04-27 18:42:57 +0500

    feat: logout products from localstorage

--- js/app.js
+++ js/app.js
@@ -8,91 +8,113 @@ let loginEmail = document.querySelectorAll("#login-email")[0]
 let loginPassword = document.querySelectorAll("#login-password")[0]
 let signinErrorMsg = document.querySelectorAll("#signin-error-msg")[0]
 let loginBtn = document.querySelectorAll("#login-btn")[0]
-let productsContainer = document.querySelectorAll("#products-container")[0]
+let productsContainer = document.querySelectorAll("#products-container")
+
 
 let userArr = JSON.parse(localStorage.getItem("users")) || []
+
+
+
+// products for localStorage start
 let products = [
-    {
-        id : crypto.randomUUID(),
-        name : "T-shirt with Tape Details",
-        price : 120,
-        imgUrl : "/assets/images/products/T-SHIRT_WITH_TAPE_DETAILS.png"
-    },
-    {
-        id : crypto.randomUUID(),
-        name : "SKINNY FIT JEANS",
-        price : 260,
-        discoutPercent : 20,
-        imgUrl : "https://shop-co-js.vercel.app/assets/images/products/CHECKERED_SHIRT.png"
-    },
-    {
-        id : crypto.randomUUID(),
-        name : "CHECKERED SHIRT",
-        price : 180,
-        imgUrl : "https://shop-co-js.vercel.app/assets/images/products/Checkered_shirt.png"
-    }
-    ,{
-        id : crypto.randomUUID(),
-        name : "SLEEVE STRIPED T-SHIRT",
-        price : 160,
-        discoutPercent : 30,
-        imgUrl : "https://shop-co-js.vercel.app/assets/images/products/Vertical_striped_shirt.png"
-    }
+  {
+    id: crypto.randomUUID(),
+    name: "T-shirt with Tape Details",
+    price: 120,
+    imgUrl: "/assets/images/products/image-7.png"
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "SKINNY FIT JEANS",
+    price: 260,
+    discountPercent: 20,
+    imgUrl: "/assets/images/products/image-5.png"
+  },
+  {
+    id: crypto.randomUUID(),
+    name: "CHECKERED SHIRT",
+    price: 180,
+    imgUrl: "/assets/images/products/image-9.png"
+  }
+  , {
+    id: crypto.randomUUID(),
+    name: "SLEEVE STRIPED T-SHIRT",
+    price: 160,
+    discountPercent: 30,
+    imgUrl: "/assets/images/products/image-10.png"
+  }
 ]
+// products for localStorage end
 
+// set products to localstorage
+localStorage.setItem("products", JSON.stringify(products))
+
+
+
+
+
+// products array from localstorage
+let ProductsArr = JSON.parse(localStorage.getItem("products"))
+
+
+
+
+
+
+// signup function
 function signUp() {
-    let user = {};
-    user.name = signupUserName.value.toLowerCase();
-    user.email = signupEmail.value.toLowerCase();
-    user.password = signupPassword.value.toLowerCase();
-    let emailExist = userArr.some(check => check.email == user.email)
-    if(user.name == ""){
-        setTimeout(() => {
-            errorMsg.innerHTML = ""
-        }, 3000);
-        errorMsg.innerHTML = "Please Enter Your Name."
-        return
-    }
-    if (emailExist) {
-        setTimeout(() => {
-            errorMsg.innerHTML = ""
-        }, 3000);
-        errorMsg.innerHTML = "Oops! This email is already registered."
-        return
-    }
-    if (!user.email.includes("@")) {
-        setTimeout(() => {
-            errorMsg.innerHTML = ""
-        }, 3000)
-        errorMsg.innerHTML = "⚠️ Please include an '@' in your email."
-        return
-    }
-    if (user.password.length <= 8) {
-        setTimeout(() => {
-            errorMsg.innerHTML = ""
-        }, 3000)
-        errorMsg.innerHTML = "🔒 Password must be at least 8 characters long."
-        return
-    }
-    if (!terms.checked) {
-        setTimeout(() => {
-            errorMsg.innerHTML = ""
-        }, 3000)
-        errorMsg.innerHTML = "❗ Acceptance of Terms is required."
-        return
-    }
+  let user = {};
+  user.name = signupUserName.value.toLowerCase();
+  user.email = signupEmail.value.toLowerCase();
+  user.password = signupPassword.value.toLowerCase();
+  let emailExist = userArr.some(check => check.email == user.email)
+  if (user.name == "") {
+    setTimeout(() => {
+      errorMsg.innerHTML = ""
+    }, 3000);
+    errorMsg.innerHTML = "Please Enter Your Name."
+    return
+  }
+  if (emailExist) {
+    setTimeout(() => {
+      errorMsg.innerHTML = ""
+    }, 3000);
+    errorMsg.innerHTML = "Oops! This email is already registered."
+    return
+  }
+  if (!user.email.includes("@")) {
+    setTimeout(() => {
+      errorMsg.innerHTML = ""
+    }, 3000)
+    errorMsg.innerHTML = "⚠️ Please include an '@' in your email."
+    return
+  }
+  if (user.password.length <= 8) {
+    setTimeout(() => {
+      errorMsg.innerHTML = ""
+    }, 3000)
+    errorMsg.innerHTML = "🔒 Password must be at least 8 characters long."
+    return
+  }
+  if (!terms.checked) {
+    setTimeout(() => {
+      errorMsg.innerHTML = ""
+    }, 3000)
+    errorMsg.innerHTML = "❗ Acceptance of Terms is required."
+    return
+  }
 
 
-    userArr.push(user)
-    localStorage.setItem("users", JSON.stringify(userArr))
+  userArr.push(user)
+  localStorage.setItem("users", JSON.stringify(userArr))
 
 
-    signupUserName.value = ""
-    signupEmail.value = ""
-    signupPassword.value = ""
-    terms.checked = false
+  signupUserName.value = ""
+  signupEmail.value = ""
+  signupPassword.value = ""
+  terms.checked = false
 
-    signUpBtn.innerHTML = `
+  signUpBtn.innerHTML = `
     ⏳ Redirecting
     <span class="loading-dots">
       <span class="dot">.</span>
@@ -100,34 +122,41 @@ function signUp() {
       <span class="dot">.</span>
     </span>
   `
-    setTimeout(() => {
-        location.href = "/signin.html"
+  setTimeout(() => {
+    location.href = "/signin.html"
 
-    }, 2000)
+  }, 2000)
 }
 
 
+
+
+
+
+
+// login function
 function logIn() {
 
-    let usersData = JSON.parse(localStorage.getItem("users"))
-    let emailExist = usersData.some(check => check.email === loginEmail.value.toLowerCase())
-    let passwordExist = usersData.some(check => check.password === loginPassword.value.toLowerCase())
-    if (!emailExist) {
-        setTimeout(() => {
-            signinErrorMsg.innerHTML = ""
-        }, 3000)
-        signinErrorMsg.innerHTML = `⚠️ Email not registered.`
-        return
-    }
-    if (!passwordExist) {
-        setTimeout(() => {
-            signinErrorMsg.innerHTML = ""
-        }, 3000)
-        signinErrorMsg.innerHTML = `❌ Wrong password. Try again.`
-        return
-    }
+  let usersData = JSON.parse(localStorage.getItem("users")) || []
+  let emailExist = usersData.some(check => check.email === loginEmail.value.toLowerCase())
+  let passwordExist = usersData.some(check => check.password === loginPassword.value.toLowerCase())
+  if (!emailExist) {
+    setTimeout(() => {
+      signinErrorMsg.innerHTML = ""
+    }, 3000)
+    signinErrorMsg.innerHTML = `⚠️ Email not registered.`
+    return
+  }
+  if (!passwordExist) {
+    setTimeout(() => {
+      signinErrorMsg.innerHTML = ""
+    }, 3000)
+    signinErrorMsg.innerHTML = `❌ Wrong password. Try again.`
+    return
+  }
 
-    loginBtn.innerHTML = `
+
+  loginBtn.innerHTML = `
     ⏳ Redirecting
     <span class="loading-dots">
       <span class="dot">.</span>
@@ -135,25 +164,47 @@ function logIn() {
       <span class="dot">.</span>
     </span>
   `
-    setTimeout(() => {
-        location.href = "/index.htm"
+  localStorage.setItem("isLoggedIn", "true");
 
-    }, 2000)
-    console.log(usersData)
+  setTimeout(() => {
+    location.href = "/index.htm"
+
+  }, 2000)
 
 }
 
 
-function ProductList (products){
-for(let i = 0; i<products.length; i++){
-    let productCart = makeCart(products[i])
-    productsContainer.innerHTML += productCart
-}
-}
-ProductList(products)
+// logout function
+function logout(Ele){
+  const confirmLogout = confirm("Are you sure you want to log out?");
 
-function makeCart (Product){
-    return `
+  if(confirmLogout){
+  Ele.children[0].innerHTML = `
+    <span class="spinner"></span> 
+    logOut
+  `
+  setTimeout(() => {
+    localStorage.removeItem("isLoggedIn"),
+    location.reload();
+  }, 2000)
+ 
+  }
+}
+
+
+
+function ProductList(products) {
+  for (let i = 0; i < productsContainer.length; i++) {
+    for (let j = 0; j < products.length; j++) {
+      let productCart = makeCart(products[j])
+      productsContainer[i].innerHTML += productCart
+    }
+  }
+}
+ProductList(ProductsArr)
+
+function makeCart(Product) {
+  return `
     <div class="group relative flex-shrink-0 w-[200px] md:w-auto">
               <div
                 class="relative w-[200px] h-[200px] md:w-3xs md:h-64 rounded-2xl overflow-hidden"
@@ -193,25 +244,26 @@ function makeCart (Product){
                 </div>
                 <div class="flex gap-1.5 md:gap-2.5 items-center flex-wrap">
                   <p class="[font-family:arial] text-lg md:text-2xl font-bold">
+                    $${Product.discountPercent
+      ? Product.price * (1 - Product.discountPercent / 100)
+      : Product.price
+    }
+                  </p>
+                  ${Product.discountPercent ? `
+                    <p class="[font-family:arial] text-lg md:text-2xl font-bold line-through opacity-40">
                     $${Product.price}
                   </p>
-                  ${Product.discoutPercent ?`
-                    <p class="[font-family:arial] text-lg md:text-2xl font-bold line-through opacity-40">
-                    $ ${Product.price}
-                  </p>
                   `: ""
-                  }
-                  ${Product.discoutPercent ? `
+    }
+                  ${Product.discountPercent ? `
                   <div class="bg-red-700/10 px-2 md:px-3 py-1 rounded-full">
-                    <p class="text-[#FF3333] text-sm">-${Product.discoutPercent}%</p>
+                    <p class="text-[#FF3333] text-sm">-${Product.discountPercent}%</p>
                   </div>
                   `: ""
-                }
+    }
                 </div>
               </div>
             </div>
     `
 }
 
-
-console.log("Trying to load:", products[1].imgUrl);
